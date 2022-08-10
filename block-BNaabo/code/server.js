f77f3c470efea04457b52f869f172fff9286a035
SMITHAN KUMAR R <smithankumar404@gmail.com>
2022-08-10 20:28:51 +0530

    Refactor Builtin middleware

--- block-BNaabo/code/server.js
+++ block-BNaabo/code/server.js
@@ -1,20 +1,18 @@
 var express = require('express');
-
 var app = express();
-
+// middleware
 app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', (req,res) => {
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 app.post('/json', (req, res) => {
     console.log(req.body);
 })
 app.post('/contact', (req, res) => {
-    res.send(req.body);
+    console.log(req.body);
 });
-app.listen(3000, () => {
-    console.log('listening on port 3000');
+app.listen(4000, () => {
+    console.log('server is listening on port 4k')
 })
