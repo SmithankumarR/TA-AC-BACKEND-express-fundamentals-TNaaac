3117145da2b1d9d94fe644fe6ef4b9c57175d95b
SMITHAN KUMAR R <smithankumar404@gmail.com>
2022-08-12 14:00:23 +0530

    Refactor Express Assignment Level 1

--- block-BNaabv/server2/server.js
+++ block-BNaabv/server2/server.js
@@ -3,53 +3,55 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 
 var app = express();
+
 // middleware
+
 app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.use((req, res, next) => {
-//     res.json(res.cookie("UserName", "smithan"));
-//     next();
-
-// })
+app.use((req, res, next) => {
+    res.cookie('Userame', 'smithan');
+    next();
+});
 app.use('/users/:username', (req, res, next) => {
-    res.send(req.params.username);
+    var username = req.params.username;
+    res.send(`<h2>${username}</h2`);
     next();
-})
+});
 app.use('/admin', (req, res, next) => {
-    next("Unauthorized to access this page");
-})
+    next('Unauthorized to access this page');
+});
+
 // routes
-app.get('/', (req, res, next) => {
+
+app.get('/', (req, res) => {
     res.send(`<h2> Welcome to express </h2>`);
-    next();
-})
+});
 
-app.get('/about', (req, res, next) => {
+app.get('/about', (req, res) => {
     res.send('My name is qwerty');
-    next();
-})
+});
 
-app.post('/form', (req, res, next) => {
+app.post('/form', (req, res) => {
     res.json(req.body);
-    next();
-})
-app.post('/json', (req, res, next) => {
+});
+app.post('/json', (req, res) => {
     res.send(req.body);
-    next();
-})
-// error handler
+});
+
+// 404 middleware
+
 app.use((req, res, next) => {
     res.status(404).send('Page not Found');
-    
-})
-// client/server error
-app.use((err,req, res, next) => {
-    res.status(500).send('Server error :'+ err);
-})
-app.listen(3000, () => {
-    console.log("listening on port 3000");
-})
+});
 
+// 500 error
+
+app.use((err, req, res, next) => {
+    res.status(500).send('Server error :' + err);
+});
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
